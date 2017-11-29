fc2d4b0e0462c9e54f5d7b8626a5bbf30f28a312
Will Hawks <will@creativedistillery.com>
2017-11-29 10:45:08 -0600

    Larger logo and centered, plus animation for form elements.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,46 @@
 
 
-function fireWhenReady() {
+function addPlaceholder() {
 
   if ( $('#email_address_0').length) {
     $('#email_address_0').attr('placeholder', 'Enter Your Email Address');
   }
   else {
-    setTimeout(fireWhenReady, 100);
+    setTimeout(addPlaceholder, 100);
   }
 }
 
-$(document).ready(fireWhenReady);
+
+
+$(document).ready(addPlaceholder);
+
+
+
+function tryItNow() {
+
+  if ( $('#middleSection').visible() ) {
+    $('#joingList').slideToggle( 'fast' , function() {
+      console.log('message up');
+
+      $('#cdContactForm').delay(300).slideToggle( 'fast' );
+      console.log('form up');
+    });
+
+  } else {
+    setTimeout(tryItNow, 200);
+  }
+
+}
+
+$(document).ready(function(){
+
+    $('#joingList').slideToggle(10, function(){
+      console.log('message hidden');
+    });
+    $('#cdContactForm').slideToggle(10, function(){
+      console.log('form hidden');
+    });
+
+  tryItNow();
+
+});
