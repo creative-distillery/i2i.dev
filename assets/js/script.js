6d49e72892099ba835d4aec29b63c19a4a1f3f20
willfalcon <will@creativedistillery.com>
2017-12-05 17:02:10 -0600

    Mobile map app link

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,3 +38,41 @@ $(document).ready(addPlaceholder);
 //   tryItNow();
 //
 // });
+
+$(document).ready(function(){
+
+  function getMobileOperatingSystem() {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+      // Windows Phone must come first because its UA also contains "Android"
+    if (/windows phone/i.test(userAgent)) {
+        return "Windows Phone";
+    }
+
+    if (/android/i.test(userAgent)) {
+        return "Android";
+    }
+
+    // iOS detection from: http://stackoverflow.com/a/9039885/177710
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        return "iOS";
+    }
+
+    return "unknown";
+  }
+
+  var mobileBrowser = getMobileOperatingSystem();
+
+  switch (mobileBrowser) {
+  	case "Windows Phone":
+  	case "Android":
+  		$("#mapLink").attr("href", "https://maps.google.com/?q=1135+Kildaire+Farm+Rd,+Suite+200,+Cary,+NC+27511");
+  		break;
+  	case "iOS":
+  		$("#mapLink").attr("href", "https://maps.apple.com/?q=1135+Kildaire+Farm+Rd,+Suite+200,+Cary,+NC+27511");
+  		break;
+  	default:
+  		$("#mapLink").attr("href", "https://maps.google.com/?q=1135+Kildaire+Farm+Rd,+Suite+200,+Cary,+NC+27511");
+  }
+
+});
